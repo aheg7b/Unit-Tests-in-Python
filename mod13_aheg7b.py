627b5605e771f2d746aa3d48b24b529c276662a2
aheg7b <aheg7b@umsystem.edu>
2025-11-20 07:14:04 -0600

    Added in functions for validation

---
+++ mod13_aheg7b.py
@@ -0,0 +1,11 @@
+def is_valid_symbol(symbol: str) -> bool:
+    return
+
+def is_valid_chat_type(chat_tpe :str) -> bool:
+    return
+
+def is_valid_time_series(ts:str)->bool:
+    return
+
+def is_valid_date(date_str:str)->bool:
+    return
