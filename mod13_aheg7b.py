bd58607bd660b957de613e6049b6642d9e4925d6
aheg7b <aheg7b@umsystem.edu>
2025-11-20 07:19:35 -0600

    forgot to save file when commiting so unsure if things pushed

--- mod13_aheg7b.py
+++ mod13_aheg7b.py
@@ -2,10 +2,14 @@ def is_valid_symbol(symbol: str) -> bool:
     return symbol.isalpha() and symbol.isupper() and 1<= len(symbol) <=7
 
 def is_valid_chat_type(chat_tpe :str) -> bool:
-    return
+    return chat_tpe in ["1","2"]
 
 def is_valid_time_series(ts:str)->bool:
-    return
+    return ts in ["1","2","3","4"]
 
 def is_valid_date(date_str:str)->bool:
-    return
+    try:
+        datetime.strptime(date_str, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
